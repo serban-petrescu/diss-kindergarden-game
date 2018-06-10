72580ae085de2c9bcb97d415a29cd3ff70ea7556
serban-petrescu <serban.petr@yahoo.com>
2018-06-10 18:23:55 +0300

    Added static build result.

---
+++ js/resources.js
@@ -0,0 +1,407 @@
+game.resources = [
+    {
+        "name": "congrats_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_1",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_2",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_3",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_game_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_game_1",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "congrats_quiz_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "intro_game_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "intro_game_1",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "intro_quiz_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "intro_title",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "next",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "track_story_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "track_story_1",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "track_story_2",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "wrong_0",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "wrong_1",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "wrong_2",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "wrong_3",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "button_empty",
+        "type": "image",
+        "src": "data/img/gui/button_empty.png"
+    },
+    {
+        "name": "button_menu",
+        "type": "image",
+        "src": "data/img/gui/button_menu.png"
+    },
+    {
+        "name": "button_next_sprite",
+        "type": "image",
+        "src": "data/img/gui/button_next_sprite.png"
+    },
+    {
+        "name": "button_next",
+        "type": "image",
+        "src": "data/img/gui/button_next.png"
+    },
+    {
+        "name": "button_start_sprite",
+        "type": "image",
+        "src": "data/img/gui/button_start_sprite.png"
+    },
+    {
+        "name": "button_start",
+        "type": "image",
+        "src": "data/img/gui/button_start.png"
+    },
+    {
+        "name": "game_0_guidelines",
+        "type": "image",
+        "src": "data/img/gui/game_0_guidelines.png"
+    },
+    {
+        "name": "game_1_guidelines",
+        "type": "image",
+        "src": "data/img/gui/game_1_guidelines.png"
+    },
+    {
+        "name": "quiz_background",
+        "type": "image",
+        "src": "data/img/gui/quiz_background.png"
+    },
+    {
+        "name": "square_empty_sprite",
+        "type": "image",
+        "src": "data/img/gui/square_empty_sprite.png"
+    },
+    {
+        "name": "square_empty",
+        "type": "image",
+        "src": "data/img/gui/square_empty.png"
+    },
+    {
+        "name": "story_background",
+        "type": "image",
+        "src": "data/img/gui/story_background.png"
+    },
+    {
+        "name": "title_header",
+        "type": "image",
+        "src": "data/img/gui/title_header.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "game_0_tileset",
+        "type": "image",
+        "src": "data/img/map/game_0_tileset.png"
+    },
+    {
+        "name": "game_1_tileset",
+        "type": "image",
+        "src": "data/img/map/game_1_tileset.png"
+    },
+    {
+        "name": "north_pole_sign",
+        "type": "image",
+        "src": "data/img/objects/north_pole_sign.png"
+    },
+    {
+        "name": "player",
+        "type": "image",
+        "src": "data/img/objects/player.png"
+    },
+    {
+        "name": "animals_correct_0",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_0.png"
+    },
+    {
+        "name": "animals_correct_1",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_1.png"
+    },
+    {
+        "name": "animals_correct_2",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_2.png"
+    },
+    {
+        "name": "animals_correct_3",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_3.png"
+    },
+    {
+        "name": "animals_correct_4",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_4.png"
+    },
+    {
+        "name": "animals_correct_5",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_correct_5.png"
+    },
+    {
+        "name": "animals_wrong_0",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_0.png"
+    },
+    {
+        "name": "animals_wrong_1",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_1.png"
+    },
+    {
+        "name": "animals_wrong_10",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_10.png"
+    },
+    {
+        "name": "animals_wrong_11",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_11.png"
+    },
+    {
+        "name": "animals_wrong_12",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_12.png"
+    },
+    {
+        "name": "animals_wrong_13",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_13.png"
+    },
+    {
+        "name": "animals_wrong_14",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_14.png"
+    },
+    {
+        "name": "animals_wrong_15",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_15.png"
+    },
+    {
+        "name": "animals_wrong_16",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_16.png"
+    },
+    {
+        "name": "animals_wrong_17",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_17.png"
+    },
+    {
+        "name": "animals_wrong_2",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_2.png"
+    },
+    {
+        "name": "animals_wrong_3",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_3.png"
+    },
+    {
+        "name": "animals_wrong_4",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_4.png"
+    },
+    {
+        "name": "animals_wrong_5",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_5.png"
+    },
+    {
+        "name": "animals_wrong_6",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_6.png"
+    },
+    {
+        "name": "animals_wrong_7",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_7.png"
+    },
+    {
+        "name": "animals_wrong_8",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_8.png"
+    },
+    {
+        "name": "animals_wrong_9",
+        "type": "image",
+        "src": "data/img/objects/quiz/animals_wrong_9.png"
+    },
+    {
+        "name": "ship",
+        "type": "image",
+        "src": "data/img/objects/ship.png"
+    },
+    {
+        "name": "animals_sprites_0",
+        "type": "image",
+        "src": "data/img/objects/story/animals_sprites_0.png"
+    },
+    {
+        "name": "animals_sprites_1",
+        "type": "image",
+        "src": "data/img/objects/story/animals_sprites_1.png"
+    },
+    {
+        "name": "animals_sprites_2",
+        "type": "image",
+        "src": "data/img/objects/story/animals_sprites_2.png"
+    },
+    {
+        "name": "animals_sprites_3",
+        "type": "image",
+        "src": "data/img/objects/story/animals_sprites_3.png"
+    },
+    {
+        "name": "clothes_sprite_0",
+        "type": "image",
+        "src": "data/img/objects/story/clothes_sprite_0.png"
+    },
+    {
+        "name": "clothes_sprite_1",
+        "type": "image",
+        "src": "data/img/objects/story/clothes_sprite_1.png"
+    },
+    {
+        "name": "clothes_sprite_2",
+        "type": "image",
+        "src": "data/img/objects/story/clothes_sprite_2.png"
+    },
+    {
+        "name": "clothes_sprite_3",
+        "type": "image",
+        "src": "data/img/objects/story/clothes_sprite_3.png"
+    },
+    {
+        "name": "clothes_sprites",
+        "type": "image",
+        "src": "data/img/objects/story/clothes_sprites.png"
+    },
+    {
+        "name": "location",
+        "type": "image",
+        "src": "data/img/objects/story/location.png"
+    },
+    {
+        "name": "game_data",
+        "type": "json",
+        "src": "data/json/game_data.json"
+    },
+    {
+        "name": "game_0_info",
+        "type": "json",
+        "src": "data/json/game/game_0_info.json"
+    },
+    {
+        "name": "game_1_info",
+        "type": "json",
+        "src": "data/json/game/game_1_info.json"
+    },
+    {
+        "name": "quiz_0_script",
+        "type": "json",
+        "src": "data/json/quiz/quiz_0_script.json"
+    },
+    {
+        "name": "story_0_script",
+        "type": "json",
+        "src": "data/json/story/story_0_script.json"
+    },
+    {
+        "name": "story_1_script",
+        "type": "json",
+        "src": "data/json/story/story_1_script.json"
+    },
+    {
+        "name": "story_2_script",
+        "type": "json",
+        "src": "data/json/story/story_2_script.json"
+    },
+    {
+        "name": "game_0_map",
+        "type": "tmx",
+        "src": "data/map/story/game_0_map.tmx"
+    },
+    {
+        "name": "game_1_map",
+        "type": "tmx",
+        "src": "data/map/story/game_1_map.tmx"
+    }
+];
